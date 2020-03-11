2e0bf61c5dd18fda6a5714c6a0cdf9956efafb58
Matthew Upson <matthew.a.upson@gmail.com>
2020-03-11 12:42:10 -0300

    new: Add prodigy entrypoint tests

--- tests/prodigy/test_prodigy_entrypoints.py
+++ tests/prodigy/test_prodigy_entrypoints.py
@@ -1,20 +1,50 @@
-from deep_reference_parser.prodigy import annotate_numbered_references
-from deep_reference_parser.prodigy import prodigy_to_tsv
-from deep_reference_parser.prodigy import reach_to_prodigy
-from deep_reference_parser.prodigy import reference_to_token_annotations
+"""Simple tests that entrypoints run. Functionality is tested in other more 
+specific tests
+"""
+
+import os
+
+import pytest
+
+from deep_reference_parser.prodigy import (
+    annotate_numbered_references,
+    prodigy_to_tsv,
+    reach_to_prodigy,
+    reference_to_token_annotations,
+)
+
+from .common import TEST_NUMBERED_REFERENCES, TEST_TOKEN_LABELLED, TEST_REACH
 
 
-def test_annotate_numbered_references_entrypoint():
-    pass
+@pytest.fixture(scope="session")
+def tmpdir(tmpdir_factory):
+    return tmpdir_factory.mktemp("data")
 
 
-def test_prodigy_to_tsv():
-    pass
+def test_annotate_numbered_references_entrypoint(tmpdir):
+    annotate_numbered_references(
+        TEST_NUMBERED_REFERENCES, os.path.join(tmpdir, "references.jsonl")
+    )
 
 
-def test_reach_to_prodigy():
-    pass
+def test_prodigy_to_tsv(tmpdir):
+    prodigy_to_tsv(
+        TEST_TOKEN_LABELLED,
+        os.path.join(tmpdir, "tokens.tsv"),
+        respect_lines=False,
+        respect_docs=True,
+    )
 
 
-def test_reference_to_token_annotations():
-    pass
+def test_reach_to_prodigy(tmpdir):
+    reach_to_prodigy(
+        TEST_REACH,
+        os.path.join(tmpdir, "prodigy.jsonl")
+    )
+
+
+def test_reference_to_token_annotations(tmpdir):
+    reference_to_token_annotations(
+        TEST_NUMBERED_REFERENCES,
+        os.path.join(tmpdir, "tokens.jsonl")
+    )
